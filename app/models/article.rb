044ff76c4fbe588c1c1636d6437893f7ca8721a2
faraanayyaz <faran@thesoftaims.com>
2021-04-08 16:31:41 +0500

    Validations Update

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: {:message => "Already Taken"}
     validates :body, presence: true, length: {minimum: 10}
 end
